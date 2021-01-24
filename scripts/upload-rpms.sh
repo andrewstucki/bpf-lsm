a1f9cf083558e0c62fb75a1d29b6069780a59daa
Andrew Stucki <andrew.stucki@elastic.co>
2021-01-23 19:49:06 -0500

    update script

--- scripts/upload-rpms.sh
+++ scripts/upload-rpms.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
 aws s3 mb s3://andrewstucki-bpf-lsm
-repomanage --old --keep=2 -c "$REPO" | xargs rm -f
-createrepo -q ${1:+--update -c "${REPO}.cache"} --retain-old-md=3 --workers=3 --unique-md-filenames --database -d "$REPO" || die "Could not createrepo in '$REPO'"
-aws s3 sync ${AWSCLI_OPTIONS[@]} "$REPO" "$REPO_URL" || die "Could not upload repo to s3"
+createrepo -q --retain-old-md=3 --workers=3 --unique-md-filenames --database -d rpms
+aws s3 sync rpms s3://andrewstucki-bpf-lsm --acl public-read
